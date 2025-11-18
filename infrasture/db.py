65dc337a438c22df345e69096502eb8dbef984be
juanhp324 <juanhp324@gmail.com>
2025-11-18 17:34:19 -0400

    Le arreglé el problema con la variables de entorno

--- infrasture/db.py
+++ infrasture/db.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 import threading
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Configuración de la conexión
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongo")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "Forporea")
 
 # Configuración optimizada para evitar timeouts
